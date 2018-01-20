0495c5bc33369a841e51e8c274b3057965d2e47f
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-20 23:07:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 
 //describe('addElementToEndOfArray(array, element)
 function addElementToEndOfArray(array, element) {
-     newArr = 
+     newArr = [...array]
      return array
     }
 
