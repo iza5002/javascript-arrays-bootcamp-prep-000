f9fff97b8e631dbaa8e2a9647289fc690a3e497e
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-20 22:57:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,41 +5,41 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
   
 
 
-//describe('addElementToBeginningOfArray(array, element)', () => {
+//describe('addElementToBeginningOfArray(array, element)
 function addElementToBeginningOfArray(array, element) {
     return [element, ...array]
     
 }
 
-  //describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
+  //describe('destructivelyAddElementToBeginningOfArray(array, element)
 function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element)
     return array
 }
 
-//describe('addElementToEndOfArray(array, element)', () => {
+//describe('addElementToEndOfArray(array, element)
 function addElementToEndOfArray(array, element) {
      var newArr = [array, ...element]
      return newArr
     }
 
-//describe('destructivelyAddElementToEndOfArray(array, element)', () => {
+//describe('destructivelyAddElementToEndOfArray(array, element)
 function destructivelyAddElementToEndOfArray(array, element) {
     array.push(element)
     return array
     }
 
-//describe('accessElementInArray(array, index)', () => {
+//describe('accessElementInArray(array, index)
 function accessElementInArray(array, index) {
       return array[index]
     }
   
-//describe('destructivelyRemoveElementFromBeginningOfArray(array)', ()=>{
+//describe('destructivelyRemoveElementFromBeginningOfArray(array)
 function destructivelyRemoveElementFromBeginningOfArray(array) {
       array.shift()
   }
 
-//describe('removeElementFromBeginningOfArray(array)', () => {
+//describe('removeElementFromBeginningOfArray(array)
 function removeElementFromBeginningOfArray(array) {
   var newArr = array.slice(1)
   return newArr
